0787ea04962c587238aab65a7ab4de3d0b53cda3
Anmol Desai <anmoldesai@Anmols-MacBook-Pro.local>
2019-12-10 15:05:55 +1100

    added more items

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -11,4 +11,21 @@ func main() {
 	} else {
 		fmt.Printf("%d is less than %d\n", y, x)
 	}
+
+	color := "red"
+
+	if color == "red" {
+		fmt.Println("Color is red")
+	} else if color == "blue" {
+		fmt.Println("Color is blue")
+	} else {
+		fmt.Println("Color is not blue or red")
+	}
+
+	switch color {
+	case "red":
+		fmt.Println("Color is red")
+	case "blue":
+		fmt.Println("Color is blue")
+	}
 }
